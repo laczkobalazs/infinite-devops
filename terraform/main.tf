c6a0cea8a316df723bc689498f3d795d153c1a8f
laczkobalazs <laczko.balazs89@gmail.com>
2021-04-19 12:01:28 +0200

    adds aws Container Registry repository to hold container image

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,12 @@ provider "aws" {
   region  = var.aws_region
   profile = var.aws_profile
 }
+
+resource "aws_ecr_repository" "ecr_repo" {
+  name                 = "infinite-devops-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
