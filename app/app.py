f6424dbf83ec1d60306a1e53a7a6b4aae10926d8
laczkobalazs <laczko.balazs89@gmail.com>
2021-04-20 21:17:55 +0200

    refactors parameters, decoupling the correct values

--- app/app.py
+++ app/app.py
@@ -1,21 +1,22 @@
 import psycopg2
 import boto3
 
-ENDPOINT = "postgresmydb.123456789012.us-east-1.rds.amazonaws.com"
-PORT = "5432"
-USR = "jane_doe"
-REGION = "us-east-1"
-DBNAME = "mydb"
-
 # gets the credentials from .aws/credentials
+
 session = boto3.Session(profile_name='twosteps_admin')
 client = session.client('ssm')
-ENDPOINT = client.get_parameter(Name='DB_ENDPOINT', WithDecryption=True)
-PORT = client.get_parameter(Name='DB_PORT', WithDecryption=True)
-USER = client.get_parameter(Name='DB_USER', WithDecryption=True)
-PASSWORD = client.get_parameter(Name='DB_PASSWORD', WithDecryption=True)
-REGION = client.get_parameter(Name='DB_REGION', WithDecryption=True)
-DBNAME = client.get_parameter(Name='DB_DBNAME', WithDecryption=True)
+ENDPOINT = client.get_parameter(Name='DB_ENDPOINT', WithDecryption=True)[
+    'Parameter']['Value']
+PORT = client.get_parameter(Name='DB_PORT', WithDecryption=True)[
+    'Parameter']['Value']
+USER = client.get_parameter(Name='DB_USER', WithDecryption=True)[
+    'Parameter']['Value']
+PASSWORD = client.get_parameter(Name='DB_PASSWORD', WithDecryption=True)[
+    'Parameter']['Value']
+REGION = client.get_parameter(Name='DB_REGION', WithDecryption=True)[
+    'Parameter']['Value']
+DBNAME = client.get_parameter(Name='DB_DBNAME', WithDecryption=True)[
+    'Parameter']['Value']
 
 try:
     conn = psycopg2.connect(host=ENDPOINT, port=PORT,
