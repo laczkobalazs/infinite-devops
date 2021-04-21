2a9995b50e9b2efdd33272be973ee35ff9bc4bb3
laczkobalazs <laczko.balazs89@gmail.com>
2021-04-22 01:40:34 +0200

    adds jenkinsfile

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -5,8 +5,8 @@ resource "aws_db_instance" "rds_postgres" {
   engine_version        = "12.5"
   instance_class        = "db.t2.micro"
   identifier            = "postgresql-db-1"
-  name                  = data.aws_db_instance.rds_database_name.value
-  username              = data.aws_db_instance.rds_database_username.value
-  password              = data.aws_db_instance.rds_database_password.value
+  name                  = data.aws_ssm_parameter.rds_database_name.value
+  username              = data.aws_ssm_parameter.rds_database_username.value
+  password              = data.aws_ssm_parameter.rds_database_password.value
   skip_final_snapshot   = true
 }
